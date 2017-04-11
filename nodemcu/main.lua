dbac81bf542b5c590ad6f2e797bcdec7cf339ee1
Deepak Adhikari <deeps.adhi@gmail.com>
2017-04-11 08:01:58 +0545

    Add Last Will and Testament

--- nodemcu/main.lua
+++ nodemcu/main.lua
@@ -6,6 +6,8 @@ tmr.alarm(0, 1000, 1, function()
 
     local node
     node = mqtt.Client(NODE_NAME, 60, "", "")
+    -- Set up Last Will and Testament
+    node:lwt("/lwt", "Oh noes! Plz! I don't wanna die!", 0, 0)
 
     -- Subscribe topic on connect
     node:on("connect", function(m)
